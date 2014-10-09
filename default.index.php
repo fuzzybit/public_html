3d7c5b0ba8acb9468440ae4d7033cf237970fb13
atucovic <fuzzybit@users.noreply.github.com>
2014-10-09 10:14:12 -0500

    added public bootstrap file and default index page

---
+++ default.index.php
@@ -0,0 +1,5 @@
+<?php
+
+	require_once("global.php");
+
+	Container::newFrontInstance("xoo/sprite/value/[VALUE]");
---
+++ global.php
@@ -0,0 +1,23 @@
+<?php
+
+	/**
+	 * This is a bootstrap file for a MVC design pattern.
+	 *
+	 * @package	FuzzyBit XOO
+	 */
+
+	if (!isset($_SERVER["HTTPS"]) && $_SERVER["SERVER_PORT"] != 443) {
+		header("HTTP/1.1 403 Forbidden");
+		exit;
+	}
+
+	require_once("client/constants.php");
+
+	require_once("$path/php5/Container.php");
+	require_once("$path/php5/APICaller.php");
+	require_once("$path/php5/logic/HTTPStatusCodes.php");
+
+	require_once("$path/applications/models/IController.php");
+	require_once("$path/applications/models/front.php");
+	require_once("$path/applications/models/view.php");
+	require_once("$path/applications/controllers/front/layout.php");
