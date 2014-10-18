e4be94fcca0f1e8dba39772dc2d9745acbd9b83c
atucovic <fuzzybit@users.noreply.github.com>
2014-10-18 11:32:56 -0500

    refactored URI; edited comments

--- client/javascript/default.constants.js
+++ client/javascript/default.constants.js
@@ -6,23 +6,31 @@
  */
 
 /**
- * HTTPS protocol
+ * PROTOCOL
  */
 var protocol = "https";
 
 /**
  * HOST
  */
-var host = "[HOST]/";
+var host = "[HOST]";
 
 /**
- * These are constants that need to be factored out possibly by making ajax calls to the API.
+ * CLIENT
+ */
+var client = "[CLIENT PATH]";
+
+/**
+ * TO DO: These are constants that need to be factored out possibly by making ajax calls to the API.
+ *
+ * "/xoo/sprite/value/17/node/7904.1"
+ * id_1045_1
  */
 
 /**
  * REFERENCED BY core.js
  */
-var signatureDimensionsURI = protocol + "://" + host + "xoo/sprite/value/17/node/7904.1";
+var signatureDimensionsURI = protocol + "://" + host + "/" + client + "/xoo/sprite/value/17/node/7904.1";
 
 /**
  * REFERENCED BY js.js
